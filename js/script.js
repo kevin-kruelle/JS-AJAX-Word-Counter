a65545da7c9c3872510539bb7e768fd153aed8f3
Kevin Kruelle <kkruelle2@gmail.com>
2021-03-03 14:15:39 -0500

    added steps to follow

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 console.log('connected...')
+
+/* STEPS TO FOLLOW
+*1. Create XHR object
+*2. Open request
+*3. Send request (won't send yet we still need to write callback function)
+*4.Write .onreadystatechange callback function
+*5.store .txt file in an associative array(each word will be stored only ONCE**)
+*6. A value should be stored for how many repeat words there are.
+*7. Output all words in the associative array in the map with their occurences.
+*8. Create an object and store word count of associative array result
+*9. Store count of unique words, most/least used words and the longest word in object.
+*10. Create a method that accpets a string value and returns the word count of that string value.
+*/
