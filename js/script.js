0774acd47feaa561bd9c160a00c80c5ba107b9c3
Kevin Kruelle <kkruelle2@gmail.com>
2021-03-04 15:32:35 -0500

    update

--- js/script.js
+++ js/script.js
@@ -14,23 +14,45 @@ console.log('connected...')
 *11. Create a method that accpets a string value and returns the word count of that string value.
 */
 
+//Create new XHR object
 let xhr = new XMLHttpRequest()
 
+//Create callback function to display Green Eggs & Ham .txt file on webpage.
 xhr.onreadystatechange = function (){
     if (this.readyState == 4 && this.status == 200) {
        parseResponse(this.responseText);
     }
 }
 
+//Open request.
 xhr.open("GET", "https://class-demo-files-and-resources.s3.amazonaws.com/Green-Eggs-And-Ham.txt",true)
+
+//Send request.
 xhr.send()
 
-
-let parseResponse = (string) => {
+//Function determines where .txt file of green eggs & ham is displayed on the webpage.
+let parseResponse = function(string) {
     document.getElementById("demo").innerHTML = string;
-    let splitResponse = string.split(' ');
-        console.log(splitResponse)
-    let checked = {}
+
+    //Split long string into multiple strings and stored in array splitResponse
+    
+    
+    let splitResponse = string.split('\n');
+
+    let splitResponseBySpace = (string) => {
+        for (let i = 0; i < string.length; i++) {
+            string[i] = splitResponse(string[i], ' ')
+        }
+        return string
+    }
+    let parseResponse = splitResponseBySpace(splitResponse)
+    
+    
+    // splitResponseSplit = string.split(' ')
+        console.log(parseResponse)
+
+    //Checked to see how many times a string occurs in the array and logged output.
+    let checked = {};
     for (let word of splitResponse) {
         if (checked[word]) {
             checked[word] = checked[word] +1;
@@ -38,7 +60,23 @@ let parseResponse = (string) => {
             checked[word] = 1;
         }
     }
-    console.log(checked)
+    //console.log(checked)
+
+ // Loop through array SplitResponse. Will determine which string is the longest and store amount of charectors in the variable lgth. Which ever string is determined to be the longest will be stored in the variable longest.
+        let lgth = 0;
+        let longest;
+    for (let i = 0; i < splitResponse.length; i++) {
+        if (splitResponse[i].length > lgth) {
+            lgth = splitResponse[i].length;
+            longest = splitResponse[i];
+        } 
+    }
+    //console.log(longest, lgth)
+
+    let wordCount = splitReponse.length;
+    //console.log(wordCount)
 }
+
+
 
 
